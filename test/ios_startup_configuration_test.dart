0f37109246638c337be1c605b421b0bda6e62ede
Hosain Misya <hosainmisya@gmail.com>
2026-08-07 00:26:55 +0100

    Fix iOS startup AdMob configuration

---
+++ test/ios_startup_configuration_test.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('iOS Info.plist contains a valid iOS AdMob application id', () {
+    final infoPlist = File('ios/Runner/Info.plist').readAsStringSync();
+
+    expect(infoPlist, contains('<key>GADApplicationIdentifier</key>'));
+    expect(
+      infoPlist,
+      contains('ca-app-pub-3940256099942544~1458002511'),
+    );
+    expect(
+      infoPlist,
+      isNot(contains('ca-app-pub-3940256099942544~3347511713')),
+    );
+  });
+}
